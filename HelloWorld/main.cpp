83262542be270a79365261d023bd8b081fe83f1e
Kate Johnson <johnson.kat.g@gmail.com>
2023-03-23 19:32:33 -0400

    program to calculate area of a circle

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -8,6 +8,7 @@ int main()
   double radius;
   cout << "What is the radius of the circle?  ";
   cin >> radius;
-  cout << "The area of the circle is " << pow(radius, 2) * 3.14;
+  const double pi = 3.14;
+  cout << "The area of the circle is " << pi * pow(radius, 2);
   return 0;
 }
