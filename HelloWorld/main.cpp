53f1554d228bd517a31d74612fe6d1be679b3529
Kate Johnson <johnson.kat.g@gmail.com>
2023-03-23 19:31:29 -0400

    using cmath functions

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-  cout << "Enter temperature in Fahrenheit: ";
-  double fTemp;
-  cin >> fTemp;
-  cout << "Temperature in Celsius: " << (fTemp - 32) * 5 / 9;
+  double radius;
+  cout << "What is the radius of the circle?  ";
+  cin >> radius;
+  cout << "The area of the circle is " << pow(radius, 2) * 3.14;
   return 0;
 }
